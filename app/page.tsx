cf1205441126d7e2d5ff924a76acf4e7dcea0e61
Plapoi_ <kopz7135@gmail.com>
2024-01-22 11:45:29 +0700

    charlotte is beautiful

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-   <div> charlotte </div>
-       
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-green-500">
+      <div> จอห์น สนมิธ หัวขวย </div>
     </main>
   );
 }
