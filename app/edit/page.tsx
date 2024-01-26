f1ea9c78fd236803faff15ff1f215d975a88b843
Elaina-AFK <khglhk@yahoo.com>
2024-01-26 15:46:58 +0700

    add link to edit page

---
+++ app/edit/page.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function EditPage() {
+  return <Link href={"/"}>This is Edit page. Click here to go back!</Link>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
+import Link from "next/link";
 import CardHolder from "./components/CardHolder";
 
 export default function Home() {
   return (
-    <main className="grid h-screen grid-rows-7 place-items-center bg-black ">
-      <h1 className="row-start-1 row-end-2 text-2xl font-bold">
-        Flashcards Project!
-      </h1>
-      <CardHolder />
-      <h2 className="row-start-7 row-end-8 mx-auto place-self-start">
-        Hover the card for answer!
-      </h2>
-    </main>
+    <>
+      <main className="grid h-screen grid-rows-7 place-items-center bg-black ">
+        <h1 className="row-start-1 row-end-2 text-2xl font-bold">
+          Flashcards Project!
+        </h1>
+        <CardHolder />
+        <h2 className="row-start-7 row-end-8 mx-auto place-self-start">
+          Hover the card for answer!
+        </h2>
+      </main>
+      <Link
+        href={"/edit"}
+        className="fixed bottom-6 right-6 aspect-square w-16 rounded-full bg-violet-700 pb-2 text-center text-6xl"
+      >
+        +
+      </Link>
+    </>
   );
 }
